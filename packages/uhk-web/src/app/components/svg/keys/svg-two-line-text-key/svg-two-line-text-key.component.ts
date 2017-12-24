dbca7aef94400678f2aa5fb446eff732aa29c9af
Ha3 <git@ha3.eu>
2017-12-24 17:39:51 +0100

    Pilot POC: Keymap localization

--- packages/uhk-web/src/app/components/svg/keys/svg-two-line-text-key/svg-two-line-text-key.component.ts
+++ packages/uhk-web/src/app/components/svg/keys/svg-two-line-text-key/svg-two-line-text-key.component.ts
@@ -11,18 +11,36 @@ export class SvgTwoLineTextKeyComponent implements OnInit {
     @Input() texts: string[];
 
     textY: number;
-    spanX: number;
+    spanXs: number[];
     spanYs: number[];
 
     constructor() {
+        this.spanXs = [];
         this.spanYs = [];
     }
 
     ngOnInit() {
         this.textY = this.height / 2;
-        this.spanX = this.width / 2;
-        for (let i = 0; i < this.texts.length; ++i) {
-            this.spanYs.push((0.75 - i * 0.5) * this.height);
+
+        if (this.texts.length != 3) {
+            this.spanXs.push(this.width / 2);
+            this.spanYs.push(0.75 * this.height);
+
+            for (let i = 1; i < this.texts.length; ++i) {
+                let lx = this.texts.length == 2 ? 0.5 : ((i - 1) / (this.texts.length - 2));
+                this.spanYs.push((0.75 - 0.5) * this.height);
+                this.spanXs.push(0.2 * this.width + lx * this.width * 0.6);
+            }
+            
+        } else {
+            this.spanXs.push(0.2 * this.width);
+            this.spanYs.push(0.75 * this.height);
+
+            this.spanXs.push(this.width / 2);
+            this.spanYs.push((0.75 - 0.5) * this.height);
+
+            this.spanXs.push(0.2 * this.width + this.width * 0.6);
+            this.spanYs.push(0.75 * this.height);
         }
     }
 }
